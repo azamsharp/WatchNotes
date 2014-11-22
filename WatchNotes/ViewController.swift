19aaa9f5d12450a69044e1bd62d3497910b0633c
Mohammad Azam <azamsharp@gmail.com>
2014-11-22 13:23:02 -0600

    Initial Commit

---
+++ WatchNotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WatchNotes
+//
+//  Created by Mohammad AZam on 11/22/14.
+//  Copyright (c) 2014 AzamSharp Consulting LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ WatchNotesTests/WatchNotesTests.swift
@@ -0,0 +1,36 @@
+//
+//  WatchNotesTests.swift
+//  WatchNotesTests
+//
+//  Created by Mohammad AZam on 11/22/14.
+//  Copyright (c) 2014 AzamSharp Consulting LLC. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class WatchNotesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
